5506d87ac0704aa0020252928aa2109a4dfe42c1
enzosorcini <enzosorcini.w@gmail.com>
2024-05-24 08:38:13 +0200

    missing shoe cards functionality

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,6 +2,7 @@ import { headerLogo } from '../assets/images'
 import { hamburger } from '../assets/icons'
 import { navLinks } from '../constants/index'
 
+// pending to add functionality to the hamburger menu
 const Nav = () => {
   return (
     <header className="padding-x py-8 absolute z-10 w-full">
@@ -12,10 +13,15 @@ const Nav = () => {
         <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
           {navLinks.map( item => (
             <li key={item.label}>
-              <a href={item.href} className="font-monserrat"></a>
+              <a href={item.href} className="font-monserrat leading-normal text-lg text-slate-gray">
+                {item.label}
+              </a>
             </li>
           ))}
         </ul>
+        <div className='lg:hidden'>
+          <img src={hamburger} alt="Hamburger" width={25} height={25} />
+        </div>
       </nav>
     </header>
   )
