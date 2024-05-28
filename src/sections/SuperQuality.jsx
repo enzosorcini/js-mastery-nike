6649cd12608777795294e8deebffda5514ba6b7d
enzosorcini <enzosorcini.w@gmail.com>
2024-05-28 20:29:49 +0200

    finished special offers section

--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -4,16 +4,19 @@ import { shoe8 } from "../assets/images"
 
 const SuperQuality = () => {
   return (
-    <section id="quality" className="w-full flex justify-center min-h-screen gap-20 max-container">
-      <div className="flex flex-col justify-start gap-5">
-        <h2 className="text-4xl font-palanquin font-bold">We Provide you <span className="text-coral-red">Super Quality</span> Shoes</h2>
-        <p className="lg:max-w-lg mt-2 font-montserrat text-slate-gray">Ensuring premium comfort and style, our meticulously crafted footwear is designed to elevate your experience, providing you with unmatched quality, innovation and a touch of elegance.</p>
-        <p className="lg:max-w-lg mt-2 font-montserrat text-slate-gray">Our dedication to detail and excellence ensures your satisfaction.</p>
-        <Button label="View details"/>  
+    <section id="about-us" className="w-full flex justify-between items-center max-lg:flex-col gap-10 max-container">
+      <div className="flex flex-col flex-1">
+        <h2 className="text-4xl font-palanquin font-bold lg:max-w-lg">We Provide you <span className="text-coral-red">Super Quality</span> Shoes</h2>
+        <p className="lg:max-w-lg mt-4 info-text">Ensuring premium comfort and style, our meticulously crafted footwear is designed to elevate your experience, providing you with unmatched quality, innovation and a touch of elegance.</p>
+        <p className="lg:max-w-lg mt-6 info-text">Our dedication to detail and excellence ensures your satisfaction.</p>
+        
+        <div className="mt-11">
+          <Button label="View details"/>
+        </div>
       </div>
       
-      <div className="">
-        <img src={shoe8} alt="shoes quality image" width={900} height={900} className=""></img>
+      <div className="flex flex-1 justify-center items-center">
+        <img src={shoe8} alt="shoes quality image" width={570} height={522} className="object-contain"></img>
       </div>
     </section>
   )
